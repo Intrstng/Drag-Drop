8638d659c222f023dc02968baecec7eb4e26b5f7
Intrstng <babich_andrey@rambler.ru>
2023-03-10 10:25:53 +0300

    feat: add find anagramms function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.querySelector('.hint-btn').addEventListener('click', function() {
   alert('Для слова "кулон" анаграммами являются слова: "клоун", "колун", "лукно", "уклон"');
 });
+
+const anagrammsArr = ['кулон', 'клоун', 'колун', 'лукно', 'уклон'];
+
+function findAnagramms() {
+  let newWord = '';
+  const container = document.querySelector('.word');
+  Array.from(document.querySelectorAll('.letter')).forEach(letter => newWord += letter.dataset.letter);
+  if (anagrammsArr.includes(newWord)) {
+    container.style.borderColor = 'rgb(0, 128, 0)';
+  } else container.style.borderColor = 'rgb(255, 0, 0)';
+}
+findAnagramms()
